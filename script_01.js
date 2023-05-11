36d9c6e517246994b3d2896a0607448283d1b566
Roxana <roxanarevilla@outlook.de>
2023-05-11 10:16:24 +0200

    / 2a. Parametrisierung + Datenübergabe von INNEN

--- script_01.js
+++ script_01.js
@@ -4,14 +4,25 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionsaufruf  (call)
-test();
+// test();
 
 // Funktionsrumpf (body) | callee
 // Funktionsdeklaration
 function test()
 {
+
    console.log("Hallo Karsten");
+}
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+ausgabeName();
+
+function ausgabeName() 
+{
+    // what happens in VEGAS ...
+   let firstName = " Sven";
+console.log("Hallo" + firstName + "!");
 }
 
 
-
+console.log(firstName); // Fehler: Scope!
