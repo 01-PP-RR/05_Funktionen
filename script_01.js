0dc39569a86eee9abe944e4f30d0b9241eacc05a
Roxana <roxanarevilla@outlook.de>
2023-05-11 09:10:49 +0200

    Functionen 01

--- script_01.js
+++ script_01.js
@@ -3,3 +3,15 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf  (call)
+test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+   console.log("Hallo Karsten");
+}
+
+
+
